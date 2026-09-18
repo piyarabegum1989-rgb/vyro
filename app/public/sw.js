b77954319f6f5262d672f5318da618a92c3d8ced
piyarabegum1989-rgb <piyarabegum1989@gmail.com>
2026-09-18 11:26:02 +0600

    Add files via upload

---
+++ app/public/sw.js
@@ -0,0 +1,8 @@
+const CACHE = 'vyro-facebook-v3-20260918';
+const CORE = ['/vyro-facebook.html', '/manifest.webmanifest', '/icons/icon-192.png', '/icons/icon-512.png'];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting())));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request).then(hit => hit || caches.match('/vyro-facebook.html'))));
+});
